f6d12c085a474f972980cb9b0e8644a8b51ce4e3
onlinedynamics25 <onlinedynamixtravel@gmail.com>
2026-04-17 15:10:38 +0100

    deleted the custom server

--- server.ts
+++
@@ -1,8 +0,0 @@
-const { Medusa } = require("@medusajs/medusa");
-const port = process.env.PORT || 9000;
-
-Medusa({}).then((app: any) => {
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Server ready on port ${port}`);
-  });
-});
