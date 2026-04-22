d8c8c98c3187072ee00b3c9beaa0fdcf53d8f776
onlinedynamics25 <onlinedynamixtravel@gmail.com>
2026-04-22 12:42:45 +0100

    worked on redisurl

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,9 +1,23 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+// import session from "express-session";
+import RedisStore from "connect-redis";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
+const redisUrl = process.env.REDIS_URL;
+
 export default defineConfig({
   projectConfig: {
+    redisUrl,
+    // sessionOptions: {
+    //   store: new RedisStore({
+    //     client: require("redis").createClient({ url: redisUrl }), // or use ioredis client
+    //     prefix: "medusa:sess:",
+    //   }),
+    //   secret: process.env.COOKIE_SECRET || "supersecret",
+    //   resave: false,
+    //   saveUninitialized: false,
+    // },
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
@@ -15,5 +29,25 @@ export default defineConfig({
   },
   admin: {
     disable: true, // Skips building the admin UI completely
+  },
+  modules: {
+    eventBus: {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        redisUrl,
+      },
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        redisUrl,
+      },
+    },
+    // locking: {
+    //   resolve: "@medusajs/locking-redis",
+    //   options: {
+    //     redisUrl,
+    //   },
+    // },
   },
 });
